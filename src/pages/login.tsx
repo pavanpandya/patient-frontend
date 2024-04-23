ef9cbc4df992dd05640176ae4ab52cd5faf9fbaa
nagopi <nagopi@iu.edu>
2024-04-22 20:33:13 -0400

    css

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,25 +7,25 @@ const Login: React.FC = () => {
       <div className="flex justify-center mt-20">
         <div className="flex flex-col items-center">
           <h1 className="font-bold text-4xl mt-2 text-blue-800">
-            Welcome to Medilink, your trusted partner for healthcare management
+            Welcome to Medilink!
           </h1>
           <div className="mt-8 max-w-md w-full">
             <div className="text-center mb-4">
-              <h2 className="text-xl font-semibold mb-4 text-blue-800">Login As:</h2>
+              <h2 className="text-xl font-semibold mb-4 text-blue-800">I am a:</h2>
               <div className="flex justify-between">
                 <Link href="/patientlogin">
                   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
-                    Hey, I am a Patient
+                    Patient
                   </button>
                 </Link>
                 <Link href="/doctorlogin">
                   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
-                    Myself Doctor
+                    Doctor
                   </button>
                 </Link>
                 <Link href="/insurancelogin">
                   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    I'm an Insurance Provider
+                    Insurance Provider
                   </button>
                 </Link>
               </div>
