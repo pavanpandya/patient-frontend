130302ad362e991e784ed04da146bd25727ef3df
fanisridhar <99811717+fanisridhar@users.noreply.github.com>
2024-04-25 00:13:35 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,8 @@ import Image from "next/image";
 import Footer from "../components/Footer";
 import main_img from "../images/main-hero-image.png";
 
-
 export default function Home() {
-  return ( 
+  return (
     <div className="flex flex-col min-h-screen">
       <main>
         <nav className="flex justify-between bg-gray-800 h-16 w-screen align-center text-milk text-slate-100 px-8">
@@ -63,8 +62,7 @@ export default function Home() {
           </div>
         </div>
       </main>
-      
+
       <Footer />
     </div>
   );
-}
